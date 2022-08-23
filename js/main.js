2d656a8aba2997629a63a709283d0a0d2b54f3c3
Eric Araya <earaya.mza@gmail.com>
2022-08-23 20:34:06 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -158,7 +158,7 @@ function agregarAlCarrito() {
 
 function botonCarrito() {
   let contenido = `<button type="button" class="btn">
-    <img src="/image/cart.png" alt="cart" width="42"><span class="badge bg-secondary">${ArticulosEnCarrito()}</span></img>
+    <img src="image/cart.png" alt="cart" width="42"><span class="badge bg-secondary">${ArticulosEnCarrito()}</span></img>
   </button>`;
 
   document.getElementById("btn-cart").innerHTML = contenido;
